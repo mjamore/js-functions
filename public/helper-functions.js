fa14623f78e0ee8a5dd1999ad128c1b66f7932b5
Michael Amore <michael.amore@dealer.com>
2016-04-30 18:30:22 -0400

    Change form action URL from hard coded HTTP to protocol agnostic URL.

--- public/helper-functions.js
+++ public/helper-functions.js
@@ -1,5 +1,3 @@
-var functions = [getRandomHexColor,getRandomInteger,getOrdinalIndicator,shuffleArray];
+var functions = [getRandomHexColor,getRandomInteger];
 var getRandomHexColor = function() {return '#'+('00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6);}
 var getRandomInteger = function(min,max) {return Math.floor(Math.random() * (max - min + 1)) + min;}
-var getOrdinalIndicator = function(number) {var string = ""; if(number == 11 || number == 12 || number == 13) {	string = "th"; } else {	var str = number.toString(), digit = str.substr(str.length -1);	if( digit == "1" ) { string = "st";	} else if( digit == "2" ) {	string = "nd"; } else if( digit == "3" ) { string = "rd"; } else { string = "th"; }	} return string;}
-var shuffleArray = function(array) {var currentIndex = array.length, temporaryValue, randomIndex;	while (0 !== currentIndex) { randomIndex = Math.floor(Math.random() * currentIndex); currentIndex -= 1;	temporaryValue = array[currentIndex]; array[currentIndex] = array[randomIndex];	array[randomIndex] = temporaryValue; } return array;}
